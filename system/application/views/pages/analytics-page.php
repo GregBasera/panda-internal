e429ade759c9fb0cccafc0d80ad141cf7292ad5c
Greg <gbasera@gbox.adnu.edu.ph>
2020-01-07 14:53:44 +0800

    optional ajax requests

--- system/application/views/pages/analytics-page.php
+++ system/application/views/pages/analytics-page.php
@@ -4,6 +4,7 @@
 
 <div class="container">
   <div class="jumbotron my-3 py-4">
+    <button class="btn btn-primary" type="button" name="button" id="dailyTranBtn" onclick="dailyTran();">Load Daily Transactions graph</button>
     <div class="spinner-border text-dark" role="status" id="dailyTranSpin">
       <span class="sr-only">Loading...</span>
     </div>
@@ -11,10 +12,10 @@
   </div>
 
   <div class="jumbotron my-3 py-4">
+    <button class="btn btn-primary" type="button" name="button" id="topPartnersBtn" onclick="topPartners();">Load Top Partners graph</button>
     <div class="spinner-border text-dark" role="status" id="topPartnersSpin">
       <span class="sr-only">Loading...</span>
     </div>
-    <!-- <input class="form-control" type="date" name="" value=""> -->
     <canvas id="topPartners"></canvas>
     <small>
       <i class="text-danger">This graph is quite misleading: New partners doesn't have equal footing with old partners.</i>
@@ -22,6 +23,7 @@
   </div>
 
   <div class="jumbotron my-3 py-4">
+    <button class="btn btn-primary" type="button" name="button" id="barangaysBtn" onclick="barangays();">Load Per-barangays graph</button>
     <div class="spinner-border text-dark" role="status" id="barangaysSpin">
       <span class="sr-only">Loading...</span>
     </div>
--- system/assets/js/panalytics.js
+++ system/assets/js/panalytics.js
@@ -29,209 +29,218 @@ function hitSearchArray(obj) {
 }
 
 // Daily Transactions
-$.ajax({
-  type: "GET",
-  url: window.origin + "/analytics/varsForDailyTran",
-  // data: data,
-  beforeSend: function() {
-    $('#dailyTranSpin').show();
-  },
-  success: function(result) {
-    // $(location).attr('href', window.origin + '/transactions/view');
-    result = JSON.parse(result);
+function dailyTran() {
+  $('#dailyTranBtn').hide();
+  $.ajax({
+    type: "GET",
+    url: window.origin + "/analytics/varsForDailyTran",
+    // data: data,
+    beforeSend: function() {
+      $('#dailyTranSpin').show();
+    },
+    success: function(result) {
+      // $(location).attr('href', window.origin + '/transactions/view');
+      result = JSON.parse(result);
 
-    var labels = [];
-    var dataDeli = [];
-    var dataCanc = [];
-    for(var q = 0; q < result.length; q++) {
-      expDate = result[result.length-q-1]['date'].split('-');
-      labels.push(expDate[1] + '-' + expDate[2]);
+      var labels = [];
+      var dataDeli = [];
+      var dataCanc = [];
+      for(var q = 0; q < result.length; q++) {
+        expDate = result[result.length-q-1]['date'].split('-');
+        labels.push(expDate[1] + '-' + expDate[2]);
 
-      dataDeli.push(parseInt(result[result.length-q-1]['delivered']));
-      dataCanc.push(parseInt(result[result.length-q-1]['cancelled']));
-    }
+        dataDeli.push(parseInt(result[result.length-q-1]['delivered']));
+        dataCanc.push(parseInt(result[result.length-q-1]['cancelled']));
+      }
 
-    var ctx = document.getElementById('dailyTran').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Canceled',
-          data: dataCanc,
-          backgroundColor: '#FF655C',
-          borderColor: 'red',
-          borderWidth: 2
-        }, {
-          label: 'Delivered',
-          data: dataDeli,
-          backgroundColor: 'lightgreen',
-          borderColor: 'green',
-          borderWidth: 2
-        }]
-      },
-      options: {
-        legend: {
-          position: 'bottom'
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            },
-            stacked: true
+      var ctx = document.getElementById('dailyTran').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Canceled',
+            data: dataCanc,
+            backgroundColor: '#FF655C',
+            borderColor: 'red',
+            borderWidth: 2
+          }, {
+            label: 'Delivered',
+            data: dataDeli,
+            backgroundColor: 'lightgreen',
+            borderColor: 'green',
+            borderWidth: 2
           }]
         },
-        elements: {
-          line: {
-            tension: 0 // disables bezier curves
+        options: {
+          legend: {
+            position: 'bottom'
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              stacked: true
+            }]
+          },
+          elements: {
+            line: {
+              tension: 0 // disables bezier curves
+            }
+          },
+          title: {
+            display: true,
+            text: 'Daily Transactions [30-day range]'
           }
-        },
-        title: {
-          display: true,
-          text: 'Daily Transactions [30-day range]'
         }
-      }
-    });
-    $('#dailyTranSpin').hide();
-  }
-});
+      });
+      $('#dailyTranSpin').hide();
+    }
+  });
+}
 
 // Top 20 Partners
-$.ajax({
-  type: "GET",
-  url: window.origin + "/analytics/varsForTop20",
-  // data: data,
-  beforeSend: function() {
-    $('#topPartnersSpin').show();
-  },
-  success: function(result) {
-    // $(location).attr('href', window.origin + '/transactions/view');
-    result = JSON.parse(result);
+function topPartners() {
+  $('#topPartnersBtn').hide();
+  $.ajax({
+    type: "GET",
+    url: window.origin + "/analytics/varsForTop20",
+    // data: data,
+    beforeSend: function() {
+      $('#topPartnersSpin').show();
+    },
+    success: function(result) {
+      // $(location).attr('href', window.origin + '/transactions/view');
+      result = JSON.parse(result);
 
-    var labels = [];
-    var onRecord = [];
-    for(var q = 0; q < result.length; q++) {
-      labels.push(result[q]['partner_name']);
-      onRecord.push(parseInt(result[q]['trans']));
-    }
-    // console.log(labels);
+      var labels = [];
+      var onRecord = [];
+      for(var q = 0; q < result.length; q++) {
+        labels.push(result[q]['partner_name']);
+        onRecord.push(parseInt(result[q]['trans']));
+      }
+      // console.log(labels);
 
-    var ctx = document.getElementById('topPartners').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'horizontalBar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Transactions on record',
-          data: onRecord,
-          backgroundColor: 'lightgreen',
-          borderColor: 'green',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        legend: {
-          position: 'bottom'
-        },
-        scales: {
-          xAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+      var ctx = document.getElementById('topPartners').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Transactions on record',
+            data: onRecord,
+            backgroundColor: 'lightgreen',
+            borderColor: 'green',
+            borderWidth: 1
           }]
         },
-        title: {
-          display: true,
-          text: 'Top 20 Partners'
+        options: {
+          legend: {
+            position: 'bottom'
+          },
+          scales: {
+            xAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          },
+          title: {
+            display: true,
+            text: 'Top 20 Partners'
+          }
         }
-      }
-    });
-    $('#topPartnersSpin').hide();
-  }
-});
+      });
+      $('#topPartnersSpin').hide();
+    }
+  });
+}
 
 // per Barangays
-$.ajax({
-  type: 'GET',
-  url: window.origin + "/analytics/varsForPerBarang",
-  // data: data,
-  beforeSend: function() {
-    $('#barangaysSpin').show();
-  },
-  success: function(result) {
-    hits = hitSearchArray(JSON.parse(result))
+function barangays() {
+  $('#barangaysBtn').hide();
+  $.ajax({
+    type: 'GET',
+    url: window.origin + "/analytics/varsForPerBarang",
+    // data: data,
+    beforeSend: function() {
+      $('#barangaysSpin').show();
+    },
+    success: function(result) {
+      hits = hitSearchArray(JSON.parse(result))
 
-    var ctx = document.getElementById('barangays').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: [
-          "Abella",
-          "Bagumbayan N/S",
-          "Balatas",
-          "Calauag",
-          "Cararayan",
-          "Carolina",
-          "Concepcion GR/PE",
-          "Dayangdang",
-          "Del Rosario",
-          "Dinaga",
-          "Igualdad",
-          "Lerma",
-          "Liboton",
-          "Mabolo",
-          "Pacol",
-          "Panicuason",
-          "Peñafrancia",
-          "Sabang",
-          "San Felipe",
-          "San Francisco",
-          "San Isidro",
-          "Santa Cruz",
-          "Tabuco",
-          "Tinago",
-          "Triangulo",
-          "Unclassified"
-        ],
-        datasets: [{
-          label: 'Keyword Hits',
-          data: hits,
-          backgroundColor: function() {
-             var unnecessary = [];
-             for(var q = 0; q < hits.length-1; q++) {unnecessary.push('lightgreen');}
-             unnecessary.push('#FF655C');
-             return unnecessary;
-          },
-          borderColor: function() {
-            var unnecessary = [];
-            for(var q = 0; q < hits.length-1; q++) {unnecessary.push('green');}
-            unnecessary.push('red');
-            return unnecessary;
-          },
-          borderWidth: 1
-        }]
-      },
-      options: {
-        legend: {
-          position: 'bottom'
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+      var ctx = document.getElementById('barangays').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: [
+            "Abella",
+            "Bagumbayan N/S",
+            "Balatas",
+            "Calauag",
+            "Cararayan",
+            "Carolina",
+            "Concepcion GR/PE",
+            "Dayangdang",
+            "Del Rosario",
+            "Dinaga",
+            "Igualdad",
+            "Lerma",
+            "Liboton",
+            "Mabolo",
+            "Pacol",
+            "Panicuason",
+            "Peñafrancia",
+            "Sabang",
+            "San Felipe",
+            "San Francisco",
+            "San Isidro",
+            "Santa Cruz",
+            "Tabuco",
+            "Tinago",
+            "Triangulo",
+            "Unclassified"
+          ],
+          datasets: [{
+            label: 'Keyword Hits',
+            data: hits,
+            backgroundColor: function() {
+               var unnecessary = [];
+               for(var q = 0; q < hits.length-1; q++) {unnecessary.push('lightgreen');}
+               unnecessary.push('#FF655C');
+               return unnecessary;
+            },
+            borderColor: function() {
+              var unnecessary = [];
+              for(var q = 0; q < hits.length-1; q++) {unnecessary.push('green');}
+              unnecessary.push('red');
+              return unnecessary;
+            },
+            borderWidth: 1
           }]
         },
-        title: {
-          display: true,
-          text: 'Per Barangays'
+        options: {
+          legend: {
+            position: 'bottom'
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          },
+          title: {
+            display: true,
+            text: 'Per Barangays'
+          }
         }
-      }
-    });
-    console.log(myChart);
-    $('#barangaysSpin').hide();
-  }
-});
+      });
+      console.log(myChart);
+      $('#barangaysSpin').hide();
+    }
+  });
+}
 
 console.log("analytics.js loaded");
