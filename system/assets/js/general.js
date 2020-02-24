b30b4597ba07c43e116eb3495a7db93a0c3fde39
GregBasera <basera.gg@gmail.com>
2020-02-24 14:19:33 +0800

    Alteration to general.js to replace the value in the indicated elem id

--- system/assets/js/general.js
+++ system/assets/js/general.js
@@ -1,1 +1,9 @@
+var cookieArrray = document.cookie.split(";");
+for(var q = 0; q < cookieArrray.length; q++) {
+  if(cookieArrray[q].split("=")[0] == 'sticky-date') {
+    $('#for-sticky-date').val(cookieArrray[q].split("=")[1]);
+  }
+}
+console.log(document.cookie);
+
 console.log("general.js loaded");
